7562aa162a88e839f8db44238b076303c769a03e
Hoang Do Khai <khai.hoangdo@ncc.asia>
2025-07-07 18:23:01 +0700

    Fix app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ input_img = st.file_uploader("Em upload ảnh gốc ở đây nhé", type=["jpg"
 pool_imgs = st.file_uploader("Em upload ảnh con ở đây nhé, em có bấm Upload rồi bôi đen kéo thả nhé", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 cell_size = st.slider("Em chọn kích cỡ ảnh của mỗi ảnh con trong ảnh gốc nhé", min_value=5, max_value=1000, value=25)
 
-if st.button("Generate Photomosaic"):
+if st.button("Em bấm vào đây để tạo ảnh nè"):
     if input_img and pool_imgs:
         input_pil = Image.open(input_img).convert("RGB")
         st.image(input_pil, caption="Original Image", use_column_width=True)
